23d49810e65c8b177ec12e0e65248d7bd3559090
BrennanKnopf <brennanknopf@gmail.com>
2021-12-21 15:31:14 -0600

    Adding answer to question 2 to repository

--- Algorithms1.py
+++ Algorithms1.py
@@ -1,3 +1,6 @@
 word = input('Put any word here.') 
 print(word[::-1])  #this slices a word. You can also use slice to print between position of indexes i.e. [2:5] would print ste from master
 
+sentence = input('Input any sentence you want here. ')
+print(sentence)
+print(sentence.title()) #for capitalizing every word in a sentence. Words a seperated by one space.
